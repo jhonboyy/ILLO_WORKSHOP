a3bf0762ce781429d7036dc86ee5739aec270b57
Jorge <hello@jhonboy.com>
2024-08-26 12:21:43 +0100

    👌 IMPROVE - Syntax

--- js/script.js
+++ js/script.js
@@ -1,26 +1,36 @@
 let layers = document.querySelectorAll(".layer")
-let palette = ['#60014C','#FE32C5','#FF5D33','#268549','#0011A8','#04A5FF','#9FF944','#3347FF','#32FE5F','#75FFB8','#FF82E3','#F42E25','#6630FF','#FAFF00','#FF7415']
+let palette = [
+    '#a7d8b5', '#f59f01', '#f1a0b5', '#ffeb54', '#a2acd8',
+    '#e87f75', '#edd6d4', '#2ac670', '#f3f19b', '#4173b1',
+    '#00969f', '#e26b52', '#ebdca8', '#aba57b', '#aedbd3',
+    '#6ba7b8', '#0085ff', '#ff6869', '#80a7af', '#81c7ff',
+    '#f93d00', '#ababab', '#eaedeb', '#2f00fd', '#e0621f',
+    '#8c67a8', '#51ff01', '#ff0074', '#75f8ff', '#51ff01',
+    '#ff9d8f', '#f57134', '#0042ff', '#000000'
+];
 
-for (let i=0;i<layers.length;i++){
-        
+for (let i = 0; i < layers.length; i++) {
+
     layers[i].style.background = palette[i];
-    layers[i].querySelector("path").style.fill = palette[i+2];
+    layers[i].querySelector("path").style.fill = palette[i + 2];
 }
 
-document.addEventListener("mousedown", function(){
+document.addEventListener("mousedown", function() {
 
     shuffle(palette);
 
-    for (let i=0;i<layers.length;i++){
-        
+    for (let i = 0; i < layers.length; i++) {
+
         layers[i].style.background = palette[i];
-        layers[i].querySelector("path").style.fill = palette[i+2];
+        layers[i].querySelector("path").style.fill = palette[i + 2];
     }
 
 })
 
-document.addEventListener("mousemove", function(e){
-    let mouseX = e.clientX; let mouseY = e.clientY;
+document.addEventListener("mousemove", function(e) {
+    let mouseX = e.clientX;
+    let mouseY = e.clientY;
+
     layers[0].style.clipPath = `polygon(${mouseX}px ${mouseY}px, 0 ${map(mouseY, 0, window.innerHeight, 45, 20)}vh, 0 0, ${map(mouseX, 0, window.innerWidth, 45, 5)}vw 0)`;
     layers[1].style.clipPath = `polygon(${mouseX}px ${mouseY}px, ${map(mouseX, 0, window.innerWidth, 45, 5)}vw 0, ${map(mouseX, 0, window.innerWidth, 80, 50)}vw 0)`;
     layers[2].style.clipPath = `polygon(${mouseX}px ${mouseY}px, ${map(mouseX, 0, window.innerWidth, 80, 50)}vw 0, 100vw 0, 100vw ${map(mouseY, 0, window.innerHeight, 40, 10)}vh)`;
@@ -28,21 +38,25 @@ document.addEventListener("mousemove", function(e){
     layers[4].style.clipPath = `polygon(${mouseX}px ${mouseY}px, 100vw ${map(mouseY, 0, window.innerHeight, 40, 90)}vh, 100vw 100vh, ${map(mouseX, 0, window.innerWidth, 60, 95)}vw 100vh)`;
     layers[5].style.clipPath = `polygon(${mouseX}px ${mouseY}px, ${map(mouseX, 0, window.innerWidth, 60, 95)}vw 100vh, ${map(mouseX, 0, window.innerWidth, 50, 20)}vw 100vh)`;
     layers[6].style.clipPath = `polygon(${mouseX}px ${mouseY}px, ${map(mouseX, 0, window.innerWidth, 50, 20)}vw 100vh, ${map(mouseX, 0, window.innerWidth, 15, 5)}vw 100vh)`;
-    layers[7].style.clipPath = `polygon(${mouseX}px ${mouseY}px, ${map(mouseX, 0, window.innerWidth, 15, 5)}vw 100vh, 0 100vh, 0 ${map(mouseY, 0, window.innerHeight, 45, 20)}vh)`
-})
+    layers[7].style.clipPath = `polygon(${mouseX}px ${mouseY}px, ${map(mouseX, 0, window.innerWidth, 15, 5)}vw 100vh, 0 100vh, 0 ${map(mouseY, 0, window.innerHeight, 45, 20)}vh)`;
+});
+
+
 
 
 function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
+    let currentIndex = array.length,
+        randomIndex;
     while (currentIndex != 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]
+        ];
     }
     return array;
-  }
+}
 
-  function map(value, low1, high1, low2, high2) {
+function map(value, low1, high1, low2, high2) {
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
